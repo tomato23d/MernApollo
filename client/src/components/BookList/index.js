8b81d0b4a38d8b7e8e8a5e1876053687fe1afe73
tomato23d <anna.a.art@gmail.com>
2023-07-17 21:31:12 +1000

    routes are updated

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -14,18 +14,15 @@ const BookList = ({ books, title }) => {
           books.map((book) => (
             <div key={book} className="col-12 col-xl-6">
               <div className="card mb-3">
-                <h4 className="card-header bg-dark text-light p-2 m-0">
-                  {book} <br />
+                
 
                   <section className="cardbook" key={book._id}>     
                       <h5 className="card-title">{book.title} </h5>
                       <h6 className="card-title">By: {book.authors} </h6>
                       <p className="card-text">Description: {book.description}</p>
-                      <a href={book.link} className="btn btn-primary">
-                        Review this book
-                      </a>
+                     
                   </section>
-                </h4>
+              
               </div>
             </div>
           ))}
